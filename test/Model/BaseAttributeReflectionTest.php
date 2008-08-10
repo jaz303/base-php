a66d9d389a69021013154ff537815e1f5a88024c
Jason Frame <jason@magiclamp.co.uk>
2008-08-10 20:53:36 +0100

    beginnings of Model class

---
+++ test/Model/BaseAttributeReflectionTest.php
@@ -0,0 +1,18 @@
+<?php
+class MBART_Model extends Model_Base
+{
+    
+}
+
+class MBART_Model_Child extends MBART_Model
+{
+    
+}
+
+class Model_BaseAttributeReflectionTest extends Test_Unit
+{
+    public function setup() {
+        
+    }
+}
+?>
