48c36721b37d92805373cd08cd578098174d1821
Jason <jason@magiclamp.co.uk>
2008-09-27 00:14:00 +0100

    fixed formatting

--- inc/base/Money.php
+++ inc/base/Money.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('MONEY_DEFAULT_CURRENCY')) {
-	define('MONEY_DEFAULT_CURRENCY', 'GBP');
+    define('MONEY_DEFAULT_CURRENCY', 'GBP');
 }
 
 if (!defined('MONEY_DEFAULT_BANK_CLASS')) {
@@ -33,57 +33,57 @@ class Money
     }
     
     //
-	// Instance
+    // Instance
     
     private $units;
-	private $currency;
-	
-	/**
-	 * Create a new Money instance
-	 *
-	 * @param $units integer number of cents/pences/units
-	 * @param $currency current, default currency used if null
-	 */
-	public function __construct($units, $currency = null) {
-	    $this->units    = (int) $units;
-	    $this->currency = self::real_currency($currency);
-	}
-	
-	public function units() {
-		return $this->units;
-	}
-	
-	public function currency() {
-	    return $this->currency;
-	}
-	
-	public function convert_to($currency) {
-		if (self::$bank === null) {
-			throw new Error_MoneyConversion("Bank not available");
-		} else {
-			return self::$bank->convert($this, $currency);
-		}
-	}
-	
-	//
-	// Formatting
-	
-	/**
-	 * Format this currency according to a given format.
-	 *
-	 * The following substitutions are understood:
-	 * %c - currency string
-	 * %s - currency symbol
-	 * %h - currency symbol (HTML)
-	 * %u - units
-	 * %f - decimal, example: 5.99
-	 *
-	 * Any other % formats which do not match exactly options above will be
-	 * passed to sprintf, which will receive a floating point version for
-	 * formatting.
-	 */
-	public function format($string) {
-	    
+    private $currency;
+    
+    /**
+     * Create a new Money instance
+     *
+     * @param $units integer number of cents/pences/units
+     * @param $currency current, default currency used if null
+     */
+    public function __construct($units, $currency = null) {
+        $this->units    = (int) $units;
+        $this->currency = self::real_currency($currency);
+    }
+    
+    public function units() {
+        return $this->units;
+    }
+    
+    public function currency() {
+        return $this->currency;
+    }
+    
+    public function convert_to($currency) {
+        if (self::$bank === null) {
+            throw new Error_MoneyConversion("Bank not available");
+        } else {
+            return self::$bank->convert($this, $currency);
+        }
+    }
+    
+    //
+    // Formatting
+    
+    /**
+     * Format this currency according to a given format.
+     *
+     * The following substitutions are understood:
+     * %c - currency string
+     * %s - currency symbol
+     * %h - currency symbol (HTML)
+     * %u - units
+     * %f - decimal, example: 5.99
+     *
+     * Any other % formats which do not match exactly options above will be
+     * passed to sprintf, which will receive a floating point version for
+     * formatting.
+     */
+    public function format($string) {
+        
         $pow = log(self::$CURRENCIES[$this->currency][0], 10);
         
         $subs = array(
@@ -98,48 +98,48 @@ class Money
         $str = sprintf($str, $this->units / self::$CURRENCIES[$this->currency][0]);
         
         return $str;
-	    
-	}
-	
-	//
-	//
-	
-	public function is_zero() {
-		return $this->units == 0;
-	}
-	
-	public function negate() {
-		return new Money(-$this->units, $this->currency);
-	}
-	
-	public function add(Money $m) {
-		if ($this->currency != $m->currency) {
-			$m = $m->convert_to($this->currency);
-		}
-		return new Money($this->units + $m->units, $this->currency);
-	}
-	
-	public function sub(Money $m) {
-		if ($this->currency != $m->currency) {
-			$m = $m->convert_to($this->currency);
-		}
-		return new Money($this->units - $m->units, $this->currency);
-	}
-	
-	public function mul($v, $round = 'up') {
-		return new Money((int) self::round($this->units * $v, $round), $this->currency);
-	}
-	
-	public function div($v, $round = 'up') {
-		return new Money((int) self::round($this->units / $v, $round), $this->currency);
-	}
-	
-	// 
-	private static $CURRENCIES = array
-	(
-		'GBP' => array(100, '£', '&pound;'),
-		'USD' => array(100, '$', '$'),
-		'EUR' => array(100, '€', '&euro;')
-	);
+        
+    }
+    
+    //
+    //
+    
+    public function is_zero() {
+        return $this->units == 0;
+    }
+    
+    public function negate() {
+        return new Money(-$this->units, $this->currency);
+    }
+    
+    public function add(Money $m) {
+        if ($this->currency != $m->currency) {
+            $m = $m->convert_to($this->currency);
+        }
+        return new Money($this->units + $m->units, $this->currency);
+    }
+    
+    public function sub(Money $m) {
+        if ($this->currency != $m->currency) {
+            $m = $m->convert_to($this->currency);
+        }
+        return new Money($this->units - $m->units, $this->currency);
+    }
+    
+    public function mul($v, $round = 'up') {
+        return new Money((int) self::round($this->units * $v, $round), $this->currency);
+    }
+    
+    public function div($v, $round = 'up') {
+        return new Money((int) self::round($this->units / $v, $round), $this->currency);
+    }
+    
+    // 
+    private static $CURRENCIES = array
+    (
+        'GBP' => array(100, '£', '&pound;'),
+        'USD' => array(100, '$', '$'),
+        'EUR' => array(100, '€', '&euro;')
+    );
 }
 ?>
