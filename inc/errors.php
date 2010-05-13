f512935d69779a68f3ec4a65d014aafe1b38be2e
Jason Frame <jason@magiclamp.co.uk>
2010-05-13 22:55:37 +0100

    Errors class for representing a collection of errors

---
+++ inc/errors.php
@@ -0,0 +1,47 @@
+<?php
+class Errors implements IteratorAggregate
+{
+    private $errors = array();
+    private $base   = array();
+    
+    public function ok() {
+        return count($this->errors) == 0 && count($this->base) == 0;
+    }
+    
+    public function add($key, $error) {
+        $this->errors[$key][] = $error;
+    }
+    
+    public function add_to_base($message) {
+        $this->base[] = $message;
+    }
+    
+    public function on($field) {
+        return isset($this->errors[$field]) ? $this->errors[$field] : false;
+    }
+    
+    public function first_on($field) {
+        return isset($this->errors[$field]) ? $this->errors[$field][0] : false;
+    }
+    
+    public function full_messages() {
+        $messages = array();
+        foreach ($this->errors as $field => $errors) {
+            $field = Inflector::humanize($field);
+            foreach ($errors as $message) {
+                if ($message[0] == '^') {
+                    $messages[] = substr($message, 1);
+                } else {
+                    $messages[] = "$field $message";
+                }
+            }
+        }
+        foreach ($this->base as $b) $messages[] = $b;
+        return $messages;
+    }
+    
+    public function getIterator() {
+        return new ArrayIterator($this->full_messages());
+    }
+}
+?>
